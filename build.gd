2f6ef4c2c8473a77367699315b77816a0dd12370
l7u7ch <33632911+l7u7ch@users.noreply.github.com>
2024-07-30 15:08:09 +0900

    メインファイルのパスを修正

--- build.gd
+++ build.gd
@@ -2,7 +2,7 @@
 extends EditorPlugin
 
 func _enter_tree():
-	add_custom_type("GPlot", "Control", preload("GPlot.cs"), null)
+	add_custom_type("GPlot", "Control", preload("src/GPlot.cs"), null)
 
 func _exit_tree():
 	remove_custom_type("GPlot")
