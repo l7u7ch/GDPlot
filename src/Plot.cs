ad0fe70ed3093a8d68ac668023d36b346b2c8ec4
l7u7ch <33632911+l7u7ch@users.noreply.github.com>
2024-08-05 07:03:07 +0900

    Gridの分割数を変数化

--- src/Plot.cs
+++ src/Plot.cs
@@ -4,14 +4,16 @@ public partial class Plot : Control
 {
     public override void _Draw()
     {
+        int N = 6;
+
         // 縦線
-        for (float x = 0; x <= Size.X; x += Size.X / 10)
+        for (float x = Size.X / N; x < Size.X; x += Size.X / N)
         {
             DrawLine(new Vector2(x, 0), new Vector2(x, Size.Y), Colors.LightSlateGray);
         }
 
         // 横線
-        for (float y = 0; y <= Size.Y; y += Size.Y / 10)
+        for (float y = Size.Y / N; y < Size.Y; y += Size.Y / N)
         {
             DrawLine(new Vector2(0, y), new Vector2(Size.X, y), Colors.LightSlateGray);
         }
