5cdbf45758cdae9af8b2a22b09b89eb4d474a98b
l7u7ch <33632911+l7u7ch@users.noreply.github.com>
2024-08-12 03:38:14 +0900

    Plot, XAxis, YAxis の Size と Position を調整

--- src/GPlot.cs
+++ src/GPlot.cs
@@ -16,16 +16,13 @@ public partial class GPlot : Control
 
     public override void _Draw()
     {
-        int YOKO = 30;
-        int TATE = 20;
-
         // Plot, XAxis, YAxis の Size と Position を更新する
-        Plot.Size = Size - new Vector2(YOKO, TATE);
-        Plot.Position = new Vector2(YOKO, 0);
-        XAxis.Size = new Vector2(Size.X - YOKO - 35, TATE);
-        XAxis.Position = new Vector2(YOKO, Size.Y - XAxis.Size.Y);
-        YAxis.Size = new Vector2(YOKO, Size.Y - TATE - 20);
-        YAxis.Position = new Vector2(0, 20);
+        Plot.Size = Size;
+        Plot.Position = Vector2.Zero;
+        XAxis.Size = new Vector2(Size.X, 0);
+        XAxis.Position = new Vector2(0, Size.Y);
+        YAxis.Size = new Vector2(0, Size.Y);
+        YAxis.Position = new Vector2(Size.X, 0);
 
         // 最大値と最小値を取得する
         Vector2 MaxValue = Plot.GetMaxValue();
