9767de39d14c56d2ef106080784320da682be8b7
l7u7ch <33632911+l7u7ch@users.noreply.github.com>
2024-08-11 23:32:29 +0900

    Plotクラスを更新

--- src/Plot.cs
+++ src/Plot.cs
@@ -1,21 +1,96 @@
+using System.Linq;
 using Godot;
+using Godot.Collections;
 
 public partial class Plot : Control
 {
+    private Array<Element> Elements = new();
+    public Color GridColor = Colors.LightSlateGray;
+    public int GridTicksX = 6;
+    public int GridTicksY = 6;
+    public bool IsGrid = true;
+
+    private void DrawGrid()
+    {
+        // 縦線を描画する
+        for (float x = 0; x <= Size.X; x += Size.X / GridTicksX)
+        {
+            DrawLine(new Vector2(x, 0), new Vector2(x, Size.Y), GridColor);
+        }
+
+        // 横線を描画する
+        for (float y = 0; y <= Size.Y; y += Size.Y / GridTicksY)
+        {
+            DrawLine(new Vector2(0, y), new Vector2(Size.X, y), GridColor);
+        }
+    }
+
     public override void _Draw()
     {
-        int N = 6;
-
-        // 縦線
-        for (float x = Size.X / N; x < Size.X; x += Size.X / N)
+        if (IsGrid)
         {
-            DrawLine(new Vector2(x, 0), new Vector2(x, Size.Y), Colors.LightSlateGray);
+            DrawGrid();
         }
 
-        // 横線
-        for (float y = Size.Y / N; y < Size.Y; y += Size.Y / N)
-        {
-            DrawLine(new Vector2(0, y), new Vector2(Size.X, y), Colors.LightSlateGray);
-        }
+        // 全ての配列を平坦化する
+        // ここの部分が GetMaxValue とか GetMinValue とかで分離すべき
+        float[] allX = Elements.Where(e => e.X != null).SelectMany(e => e.X).DefaultIfEmpty().ToArray();
+        float[] allY = Elements.Where(e => e.Y != null).SelectMany(e => e.Y).DefaultIfEmpty().ToArray();
+
+        // Elementのプロパティを更新する
+        Elements
+            .ToList()
+            .ForEach(e =>
+            {
+                e.MaxValue = GetMaxValue();
+                e.MinValue = GetMinValue();
+                e.Position = Vector2.Zero;
+                e.Size = Size;
+                e.QueueRedraw();
+            });
+    }
+
+    public Bar CreateBar()
+    {
+        Bar Bar = new();
+
+        Elements.Add(Bar);
+        AddChild(Bar);
+
+        return Bar;
+    }
+
+    public Line CreateLine()
+    {
+        Line Line = new();
+
+        Elements.Add(Line);
+        AddChild(Line);
+
+        return Line;
+    }
+
+    public Scatter CreateScatter()
+    {
+        Scatter Scatter = new();
+
+        Elements.Add(Scatter);
+        AddChild(Scatter);
+
+        return Scatter;
+    }
+
+    public Vector2 GetMaxValue()
+    {
+        float[] allX = Elements.Where(e => e.X != null).SelectMany(e => e.X).DefaultIfEmpty().ToArray();
+        float[] allY = Elements.Where(e => e.Y != null).SelectMany(e => e.Y).DefaultIfEmpty().ToArray();
+        return new Vector2(allX.Max(), allY.Max());
+    }
+
+    public Vector2 GetMinValue()
+    {
+        float[] allX = Elements.Where(e => e.X != null).SelectMany(e => e.X).DefaultIfEmpty().ToArray();
+        float[] allY = Elements.Where(e => e.Y != null).SelectMany(e => e.Y).DefaultIfEmpty().ToArray();
+        return new Vector2(allX.Min(), allY.Min());
     }
 }
